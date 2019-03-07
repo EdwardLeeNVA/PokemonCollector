b9481d27a8513d0f5062de81d3eed8d100f393e5
NathanLosee <nathan.d.losee@gmail.com>
2019-03-07 15:53:47 -0500

    Added base functionality for FrontController and MasterDispatcher

--- src/main/java/com/revature/pokemonv2/dispatcher/MasterDispatcher.java
+++ src/main/java/com/revature/pokemonv2/dispatcher/MasterDispatcher.java
@@ -1,5 +1,28 @@
 package com.revature.pokemonv2.dispatcher;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
 public class MasterDispatcher {
+	private MasterDispatcher() { }
+	private static final Logger log = Logger.getLogger(MasterDispatcher.class);
+	
+	public static void process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String[] uriStrings = request.getRequestURI().split("/");
+		String uri = uriStrings[uriStrings.length - 1];
+		
+		switch(uri) {
+		case "register":
+			break;
+		default:
+			log.error("URI not recognized");
+		}
+	}
 
 }
--- src/main/java/com/revature/pokemonv2/servlet/FrontController.java
+++ src/main/java/com/revature/pokemonv2/servlet/FrontController.java
@@ -1,28 +1,54 @@
 package com.revature.pokemonv2.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.revature.pokemonv2.dispatcher.MasterDispatcher;
 
 public class FrontController extends HttpServlet{
 	private static final long serialVersionUID = 4826138980180601133L;
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		
+		if (uri.contains("/servlet/"))
+			MasterDispatcher.process(request, response);
+		else if (uri.equals("/PokemonCollector/") || uri.equals("/PokemonCollector"))
+			request.getRequestDispatcher("/ng/index.html").forward(request, response);
+		else
+			super.doGet(request, response);
 	}
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		
+		if (uri.contains("/servlet/"))
+			MasterDispatcher.process(request, response);
+		else
+			super.doPost(request, response);
 	}
 	@Override
-	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet( req, resp);
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		
+		if (uri.contains("/servlet/"))
+			MasterDispatcher.process(request, response);
+		else
+			super.doPut(request, response);
 	}
 	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		
+		if (uri.contains("/servlet/"))
+			MasterDispatcher.process(request, response);
+		else
+			super.doDelete(request, response);
 	}
 }
 
