d70a0daecfd43110d5c30db775806bb6fa4c5556
Christopher Raleigh <craleigh318@comcast.net>
2019-03-09 11:28:13 -0500

    Updated Angular login component.

--- src/main/ngapp/src/app/components/login/login.component.ts
+++ src/main/ngapp/src/app/components/login/login.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  NO_LOGIN_FAILED = "";
+  LOGIN_FAILED = "<p>Wrong username and password.</p>";
+  currentLoginMessage = this.NO_LOGIN_FAILED;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  loginResponse(response: string) {
+    if (response.length > 0) {
+      this.loginSucceeded();
+    } else {
+      this.loginFailed();
+    }
+  }
+
+  loginSucceeded() {
+
+  }
+
+  loginFailed() {
+    this.currentLoginMessage = this.LOGIN_FAILED;
+  }
 }
