8c2e38ed69fe67a5949553e9602975889330d78e
Christopher Raleigh <craleigh318@comcast.net>
2019-03-08 20:35:27 -0500

    Update TrainerDAOImp.java

--- src/main/java/com/revature/pokemonv2/dao/TrainerDAOImp.java
+++ src/main/java/com/revature/pokemonv2/dao/TrainerDAOImp.java
@@ -13,6 +13,8 @@ import com.revature.pokemonv2.model.Trainer;
 import com.revature.pokemonv2.service.TokenService;
 import com.revature.pokemonv2.utilities.ConnectionUtility;
 
+import oracle.jdbc.OracleTypes;
+
 public class TrainerDAOImp implements TrainerDAO {
 
 	private static final TokenService token = TokenService.getInstance();
@@ -39,33 +41,36 @@ public class TrainerDAOImp implements TrainerDAO {
 	}
 
 	// Verifies via SQL whether the user login is correct
-	public Trainer verifyLogin(String username, String password) {
-		// Try with resources on the instance of ConnectionUtility
-		try (Connection conn = ConnectionUtility.getInstance().getConnection()) {
-			// Creates a new trainer
-			Trainer login = null;
-			// Call stored procedure
-			String sql = "CALL VERIFY_CREDENTIALS(?,?)";
-			// Try with resources on the PreparedStatement
-			try (PreparedStatement cs = conn.prepareStatement(sql)) {
-				cs.setString(1, username);
-				cs.setString(2, password);
-				try (ResultSet rs = cs.executeQuery()) {
-					if (rs.next()) {
-						login = new Trainer();
-						login.setUsername(username);
-						login.setUserID(1);
-						login.setCredits(rs.getInt(2));
-						login.setScore(rs.getInt(3));
-						return login;
+		public Trainer verifyLogin(String username, String password) {
+			// Try with resources on the instance of ConnectionUtility
+			try (Connection conn = ConnectionUtility.getInstance().getConnection()) {
+				// Creates a new trainer
+				Trainer login = null;
+				// Call stored procedure
+				String sql = "CALL VERIFY_CREDENTIALS(?,?,?)";
+				// Try with resources on the PreparedStatement
+				try (CallableStatement cs = conn.prepareCall(sql)) {
+					cs.setString(1, username);
+					cs.setString(2, password);
+					cs.registerOutParameter(3, OracleTypes.CURSOR);
+					cs.execute();
+					//Executing out parameters
+					try (ResultSet rs = (ResultSet) cs.getObject(3)) {
+						if (rs.next()) {
+							login = new Trainer();
+							login.setUsername(username);
+							login.setUserID(1);
+							login.setCredits(rs.getInt(2));
+							login.setScore(rs.getInt(3));
+							return login;
+						}
 					}
 				}
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+			return null;
 		}
-		return null;
-	}
 
 	//Creates a trainer
 	@Override
