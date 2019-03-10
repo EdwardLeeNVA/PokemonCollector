1be035e5028a85f15dc5d75acfffbbaf1be0a17a
dkuhn <dkuhn999@gmail.com>
2019-03-10 16:05:38 -0400

    Revert "Removed load writer since it is throwing errors.  Did manual load through logic."
    This reverts commit 83dfc949d616f7b7675f46e5bf4c8936adc33df7.

--- src/main/java/com/revature/pokemonv2/utilities/CachingUtility.java
+++ src/main/java/com/revature/pokemonv2/utilities/CachingUtility.java
@@ -2,10 +2,8 @@ package com.revature.pokemonv2.utilities;
 
 import java.util.*;
 
-import com.revature.pokemonv2.dao.DAO;
 import me.sargunvohra.lib.pokekotlin.client.PokeApi;
 import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
-import me.sargunvohra.lib.pokekotlin.model.Pokemon;
 import org.apache.log4j.Logger;
 import org.ehcache.Cache;
 import org.ehcache.CacheManager;
@@ -13,14 +11,13 @@ import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.spi.loaderwriter.CacheWritingException;
 import org.ehcache.xml.XmlConfiguration;
 
+import com.revature.pokemonv2.model.Pokemon;
 
 public class CachingUtility {
  	
 	 private final Cache<String, ArrayList> pokedexCache;
-	 private final DAO dao = new DAO();
 	 private static CachingUtility cachingUtility = new CachingUtility();
 	 final static Logger logger = Logger.getLogger(CachingUtility.class);
-	 private final int MAX_POKEDEX_SIZE = 151;
 	 
 	 
 	 private CachingUtility(){
@@ -33,12 +30,8 @@ public class CachingUtility {
 	 }
 	 
 	 public ArrayList checkCache(String username) {
-		ArrayList<com.revature.pokemonv2.model.Pokemon> returnList = this.pokedexCache.get(username);
-		if(returnList == null){
-			this.pokedexCache.put(username, loadOnMiss(username));
-			returnList = this.pokedexCache.get(username);
-		}
-		 return returnList;
+
+		 return this.pokedexCache.get(username);
 	 }
 	 
 //	 public ArrayList addToCache(String username, int poke_id) {
@@ -62,12 +55,8 @@ public class CachingUtility {
 //	 }
 
 	public ArrayList addToCache(String username, int poke_id) {
-		ArrayList<com.revature.pokemonv2.model.Pokemon> pokeList = this.pokedexCache.get(username);
-		if(pokeList == null){
-			this.pokedexCache.put(username, loadOnMiss(username));
-			pokeList = this.pokedexCache.get(username);
-		}
-		com.revature.pokemonv2.model.Pokemon temp = pokeList.remove(poke_id);
+		ArrayList<Pokemon> pokeList = (ArrayList<Pokemon>) this.pokedexCache.get(username);
+		Pokemon temp = pokeList.remove(poke_id);
 		if(temp == null){
 			temp = this.getPokemon(poke_id);
 		} else {
@@ -82,12 +71,8 @@ public class CachingUtility {
 
 	public ArrayList redeemSinglePokemon(String username, int poke_id){
 
-		ArrayList<com.revature.pokemonv2.model.Pokemon> newPokeList = this.pokedexCache.get(username);
-		if(newPokeList == null){
-			this.pokedexCache.put(username, loadOnMiss(username));
-			newPokeList = this.pokedexCache.get(username);
-		}
-		com.revature.pokemonv2.model.Pokemon temp = newPokeList.remove(poke_id);
+		ArrayList<Pokemon> newPokeList = this.pokedexCache.get(username);
+		Pokemon temp = newPokeList.remove(poke_id);
 		temp.setCount(1);
 		newPokeList.add(temp);
 
@@ -98,14 +83,11 @@ public class CachingUtility {
 	
 
 	public ArrayList redeemAllPokemon(String username){
-		ArrayList<com.revature.pokemonv2.model.Pokemon> origPokeList = this.pokedexCache.get(username);
-		if(origPokeList == null){
-			this.pokedexCache.put(username, loadOnMiss(username));
-			origPokeList = this.pokedexCache.get(username);
-		}
-		ArrayList<com.revature.pokemonv2.model.Pokemon> newPokeList = new ArrayList<>();
+		ArrayList<Pokemon> origPokeList = this.pokedexCache.get(username);
+		ArrayList<Pokemon> newPokeList = new ArrayList<>();
 		for(int i = 0; i < newPokeList.size(); i++){
-			com.revature.pokemonv2.model.Pokemon temp = origPokeList.get(i);
+
+			Pokemon temp = origPokeList.get(i);
 			temp.setCount(1);
 			newPokeList.add(temp);
 		}
@@ -132,12 +114,7 @@ public class CachingUtility {
 	 }
 	 
 	 public ArrayList getAllPokemon() {
-		 ArrayList<com.revature.pokemonv2.model.Pokemon> returnList = pokedexCache.get("red");
-		 if(returnList == null){
-		 	pokedexCache.put("red", loadOnMiss("red"));
-		 	return pokedexCache.get("red");
-		 }
-	 	return returnList;
+		 return pokedexCache.get("red");
 	 }
 	 
 	
@@ -153,8 +130,8 @@ public class CachingUtility {
 	}
 
 	// This method will increment the counter pokemon in the Pokedex for custom eviction
-	public ArrayList<com.revature.pokemonv2.model.Pokemon> incrementCacheHit(ArrayList list){
-		com.revature.pokemonv2.model.Pokemon counter = (com.revature.pokemonv2.model.Pokemon)list.remove(0);
+	public ArrayList incrementCacheHit(ArrayList list){
+		Pokemon counter = (Pokemon)list.remove(0);
 		counter.setCount(counter.getCount() + 1);
 		list.add(counter);
 		return list;
@@ -162,7 +139,7 @@ public class CachingUtility {
 
 	public com.revature.pokemonv2.model.Pokemon getPokemon(Integer poke_id){
 			PokeApi poke = new PokeApiClient();
-			Pokemon p = poke.getPokemon(poke_id);
+			me.sargunvohra.lib.pokekotlin.model.Pokemon p = poke.getPokemon(poke_id);
 
 			ArrayList<String> typeTemp = new ArrayList<>();
 			int z = 0;
@@ -180,26 +157,5 @@ public class CachingUtility {
 
 			return new com.revature.pokemonv2.model.Pokemon(p.getId(), p.getName(), p.getSprites().getFrontDefault(),(String[])typeTemp.toArray(), statTemp, cost);
 
-	}
-
-	public ArrayList<com.revature.pokemonv2.model.Pokemon> loadOnMiss(String username){
-		ArrayList<com.revature.pokemonv2.model.Pokemon> returnPokeDex = new ArrayList<>();
-		if(username.equals("red")) {
-			for (int i = 1; i <= MAX_POKEDEX_SIZE; i++ ) {
-				returnPokeDex.add(cachingUtility.getPokemon(i));
-			}
-			return returnPokeDex;
-		} else {
-			List<com.revature.pokemonv2.model.Pokemon> pokeDex = dao.getTrainerPokedex(username);
-
-			for (com.revature.pokemonv2.model.Pokemon p : pokeDex) {
-				com.revature.pokemonv2.model.Pokemon poke = cachingUtility.getPokemon(p.getId());
-				poke.setCount(p.getCount());
-				returnPokeDex.add(poke);
-			}
-			// Adds dummy pokemon to counter Cache hits
-			/*returnPokeDex.add(new Pokemon(0, 1));*/
-			return returnPokeDex;
-		}
 	}
 }
