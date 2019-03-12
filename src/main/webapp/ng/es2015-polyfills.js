54c5d9571d1521e6c3ceeaf1c4ac4873478563d7
Isaiah Duncan <Duncan.Isaiah@gmail.com>
2019-03-12 19:07:01 -0400

    Styled Landing Page

--- src/main/webapp/ng/es2015-polyfills.js
+++ src/main/webapp/ng/es2015-polyfills.js
@@ -7006,8 +7006,8 @@ for (var collections = getKeys(DOMIterables), i = 0; i < collections.length; i++
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\my_git_repos\PokemonCollectorGroup\PokemonCollector\src\main\ngapp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es2015-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js");
-module.exports = __webpack_require__(/*! C:\my_git_repos\PokemonCollectorGroup\PokemonCollector\src\main\ngapp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es2015-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Isaiah\Desktop\workspace\PokemonCollector\src\main\ngapp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es2015-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-polyfills.js");
+module.exports = __webpack_require__(/*! C:\Users\Isaiah\Desktop\workspace\PokemonCollector\src\main\ngapp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\es2015-jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es2015-jit-polyfills.js");
 
 
 /***/ })
